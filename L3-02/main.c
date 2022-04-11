fee92ea399d86f35cca9205fe198510e8344a16d
Vitor Lany <vitorlany99@gmail.com>
2022-04-11 16:44:19 -0300

    Exercicio 2

---
+++ L3-02/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float inserido = 0, lista[];
+
+    while (1) {
+        printf("Insira os numeros: ");
+        scanf("%f", inserido);
+
+        if (inserido != -1 ) {
+
+        } else {
+            break;
+        }
+    }
+
+    return 0;
+}
