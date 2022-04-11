3b798aafe862f7d812a67d9ce293f816d93aa7f0
Vitor Lany <vitorlany99@gmail.com>
2022-04-11 17:09:09 -0300

    Exercicio 2

--- L3-02/main.c
+++ L3-02/main.c
@@ -3,18 +3,24 @@
 
 int main()
 {
-    float inserido = 0, lista[];
+    float inserido = 0, lista[100];
+    int i;
 
-    while (1) {
-        printf("Insira os numeros: ");
-        scanf("%f", inserido);
+    printf("Utilize -1 como flag\n");
+    for (i = 0; 1; i++) {
+        printf("\nInsira o valor:");
+        scanf("%f", &inserido);
 
-        if (inserido != -1 ) {
-
+        if (inserido != -1) {
+            printf("Valor inserido: %.1f \n", inserido);
+            lista[i] = inserido;
         } else {
+            printf("Flag reconhecida!");
             break;
         }
     }
+
+
 
     return 0;
 }
