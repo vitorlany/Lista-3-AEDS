17ef071c4426c795b0687765c95006125cece587
Vitor Lany <vitorlany99@gmail.com>
2022-04-14 10:49:38 -0300

    Juncao (menu)

---
+++ Lista3-Todos/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int exercicio;
+    do {
+        printf("Escolha um exercicio: ");
+        printf("\n\
+        1 - Exercicio 1\n\
+        2 - Exercicio 2\n\
+        3 - Exercicio 3\n\
+        4 - Exercicio 4\n\
+        5 - Exercicio 5\n\
+        6 - Exercicio 6\n\
+        7 - Exercicio 7\n\
+        8 - Exercicio 8\n\
+        9 - Exercicio 9\n\
+        10 - Exercicio 10\n\
+        11 - Fechar\n\
+Escolha: ");
+        scanf("%i", &exercicio);
+        switch(exercicio){
+        case 1:
+
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        case 5:
+
+            break;
+
+        case 6:
+
+            break;
+
+        case 7:
+
+            break;
+
+        case 8:
+
+            break;
+
+        case 9:
+
+            break;
+
+        case 10:
+
+            break;
+        }
+    }while(exercicio != 11);
+    return 0;
+}
